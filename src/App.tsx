5a24d0d7017c717664a979360e6346c8281a87be
Marco Burman <marcoburman@gmail.com>
2023-01-10 03:55:51 +0000

    add quizStarted state; replace Button component with html button

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
+import { useState } from 'react'
 import styles from './App.module.css'
-import Button from './components/Button/Button'
 
 function App() {
+    const [quizStarted, setQuizStarted] = useState(false)
+
+    const startScreen = () => (
+        <div className={styles.startScreen}>
+            <h1 className={styles.title}>Quizzical</h1>
+            <p>What&apos;s in the box? What&apos;s in the boooox?! 🥺</p>
+            <button className={styles.startButton} onClick={() => setQuizStarted(true)}>
+                Start Quiz
+            </button>
+        </div>
+    )
+
     return (
         <div className={styles.app}>
-            <h1>Quizzical</h1>
-            <p>What&apos;s in the boooox?</p>
-            <Button text="Start Quiz" />
+            {!quizStarted && startScreen()}
+            {quizStarted && <button>Check answers</button>}
         </div>
     )
 }
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,9 +3,10 @@ import styles from './Button.module.css'
 
 interface ButtonProps {
     text: string
+    onClick: () => void
 }
 
-const Button: React.FC<ButtonProps> = ({ text }) => (
+const Button: React.FC<ButtonProps> = ({ text, onClick }) => (
     <button className={styles.button} data-testid="Button">
         {text}
     </button>
