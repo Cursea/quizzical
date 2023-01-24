37f9389050e749a128c112b3bcfe158641546f6a
Marco Burman <marcoburman@gmail.com>
2023-01-24 04:15:41 +0000

    export question logic to own Question component

--- src/components/Question/Question.tsx
+++ src/components/Question/Question.tsx
@@ -1,14 +1,29 @@
 import React from 'react'
+import { AnswerType, QuestionType } from '../../types/appTypes'
+import { shuffleArray } from '../../utils/shuffleArray'
+import Button from '../Button/Button'
 import styles from './Question.module.css'
 
 interface QuestionProps {
-    props: any
+    question: QuestionType
 }
 
-const Question: React.FC<QuestionProps> = ({ props }) => (
-    <div className={styles.question} data-testid="Question">
-        <div>Question component</div>
-    </div>
-)
+const Question: React.FC<QuestionProps> = ({ question }) => {
+    const answers: AnswerType[] = []
+    question.incorrect_answers.map((a: string) => answers.push({ answer: a, correct: false }))
+    answers.push({ answer: question.correct_answer, correct: true })
+    shuffleArray(answers)
+
+    return (
+        <div className={styles.questionContainer} data-testid="Question">
+            <h4 className={styles.questionHeader}>{question.question}</h4>
+            <ul className={styles.answers}>
+                {answers.map((answer, i) => (
+                    <Button key={i} text={answer.answer} type={answer.correct} />
+                ))}
+            </ul>
+        </div>
+    )
+}
 
 export default Question
--- src/components/Quiz/Quiz.tsx
+++ src/components/Quiz/Quiz.tsx
@@ -1,34 +1,17 @@
-import React, { ReactNode, useState } from 'react'
-import Button from '../Button/Button'
-import { shuffleArray } from '../../utils/shuffleArray'
-import { AnswerType, QuestionType } from '../../types/appTypes'
+import React from 'react'
+import { QuestionType } from '../../types/appTypes'
+import Question from '../Question/Question'
 import styles from './Quiz.module.css'
 
 interface QuizProps {
     questions: QuestionType[]
 }
 
-const question = (question: QuestionType): ReactNode => {
-    const answers: AnswerType[] = []
-    question.incorrect_answers.map((a: string) => answers.push({ answer: a, correct: false }))
-    answers.push({ answer: question.correct_answer, correct: true })
-    shuffleArray(answers)
-
-    return (
-        <div>
-            <p>{question.question}</p>
-            {answers.map((answer, i) => (
-                <Button key={i} text={answer.answer} type={answer.correct} />
-            ))}
-        </div>
-    )
-}
-
 const Quiz: React.FC<QuizProps> = ({ questions }) => {
     return (
         <div className={styles.quiz} data-testid="Quiz">
-            {questions.map((q) => (
-                <React.Fragment key={q.question}>{question(q)}</React.Fragment>
+            {questions.map((question) => (
+                <Question key={question.question} question={question} />
             ))}
         </div>
     )
