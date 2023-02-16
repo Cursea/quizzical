dcae0d768f31bfbc652977028c0473104b71ef68
Marco Burman <marcoburman@gmail.com>
2023-02-16 02:22:35 +0000

    count number of shuffle calls

--- src/utils/shuffleArray.ts
+++ src/utils/shuffleArray.ts
@@ -1,6 +1,7 @@
 import { AnswerType } from '../types/appTypes'
 
 export const shuffleArray = (array: AnswerType[]): AnswerType[] => {
+    console.count('shuffleArray() called')
     for (let i = array.length - 1; i > 0; i--) {
         const j: number = Math.floor(Math.random() * (i + 1))
         const temp = array[i]
